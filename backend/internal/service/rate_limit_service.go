4f3356f24df505fdf86e9e39ffc80afc64207de0
Nischal07bot <singhnischal953@gmail.com>
2026-03-28 16:02:29 +0530

    timeout added on requests to redis

--- backend/internal/service/rate_limit_service.go
+++ backend/internal/service/rate_limit_service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 	"fmt"
+	"time"
+
 	"github.com/nischal/rate-limiter/internal/config"
 	"github.com/nischal/rate-limiter/internal/repositories"
 )
@@ -20,6 +22,13 @@ func NewRateLimitService(repo *repositories.RateLimitRepository, cfg config.Rate
 }//new rate limit service with rate limit script and client set client should be passed on to repo
 
 func (s *RateLimitService) IsAllowed(ctx context.Context, identifier string) (bool, int64, error) {
+	ctx, cancel:= context.WithTimeout(ctx, 50*time.Millisecond)
+	defer cancel()
+	defer func() {
+		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Println("Rate Limiter check timed out for identifier:", identifier)
+		}
+	}()
 	key := fmt.Sprintf("rate_limit:%s", identifier);
 	allowed, remaining, err := s.repo.CheckLimit(
 		ctx,
